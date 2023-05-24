d23a0f65aeb602773befd18dd28d1247bed51e33
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-05-24 02:52:49 +0000

    Add vector databases

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from langchain.document_loaders import PyPDFLoader, WebBaseLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
+from langchain.indexes.vectorstores import AnnoyVectorStore
 import openai
 import os
 import tempfile
@@ -44,15 +45,23 @@ def main():
         embeddings = OpenAIEmbeddings()
         resume_embeddings = embeddings.embed_documents([str(document) for document in resume_pages])
         job_description_embeddings = embeddings.embed_documents([str(document) for document in job_description_pages])
-        news_embeddings = embeddings.embed_documents([company_news]) 
+        news_embeddings = embeddings.embed_documents([company_news])
+
+        # Create Annoy index
+        vector_db = AnnoyVectorStore(dimensions=len(resume_embeddings[0]))
+        
+        for i, embedding in enumerate(resume_embeddings):
+            vector_db.add_vector(i, embedding)
+        for i, embedding in enumerate(job_description_embeddings, start=len(resume_embeddings)):
+            vector_db.add_vector(i, embedding)
+        for i, embedding in enumerate(news_embeddings, start=len(resume_embeddings)+len(job_description_embeddings)):
+            vector_db.add_vector(i, embedding)
+        
+        vector_db.build()
 
         # Use the embeddings to generate a cover letter
         chat = ChatOpenAI()
-        context = RetrievalQA({
-            'resume': resume_embeddings,
-            'job_description': job_description_embeddings,
-            'company_news': news_embeddings
-        })  # change made here
+        context = RetrievalQA(vector_db)
         prompt = f"I am applying for a job at {company_name}."
         cover_letter = chat.generate(context, prompt)
             
