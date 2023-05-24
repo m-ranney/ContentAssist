9c54978726fc10de5c57d1ae36e8718681d5e787
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-05-24 05:28:31 +0000

    Update langchain to chat

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from langchain.document_loaders import PyPDFLoader, WebBaseLoader
 from langchain.chat_models import ChatOpenAI
-from langchain.messages import Message
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate)
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
 import openai
 import os
 import tempfile
@@ -39,22 +40,29 @@ def main():
     company_news = st.text_area('Input the recent company news here')
 
     if st.button('Generate Cover Letter'):
-        # Concatenate the resume, job description, and news to form the context
+        # Prepare the context content and prompt content
         context_content = "\n".join(str(document) for document in resume_pages + job_description_pages) + "\n" + company_news
-        context = Message(role='system', content=context_content)
-    
-        # Formulate the prompt as a user message
         prompt_content = f"""
-        I'm applying for a position at {company_name}. Given my skills and experience, which are outlined in my resume, I believe I would be a good fit. The job description for the role resonates with my professional profile. Furthermore, the recent news from the company has gotten me very excited about this opportunity.I would like to express my interest and enthusiasm for this role in a cover letter. Can you help me draft one?
+        I'm applying for a position at {company_name}. Given my skills and experience, which are outlined in my resume, I believe I would be a good fit. The job description for the role resonates with my professional profile. Furthermore, the recent news from the company has gotten me very excited about this opportunity. I would like to express my interest and enthusiasm for this role in a cover letter. Can you help me draft one?
         """
-        prompt = Message(role='user', content=prompt_content)
     
-        # Generate the cover letter using the context and prompt
-        conversation = [context, prompt]
-        cover_letter = chat.generate(conversation)
-                
+        # Define message templates for context and prompt
+        system_message_prompt = SystemMessagePromptTemplate.from_template("{context}")
+        human_message_prompt = HumanMessagePromptTemplate.from_template("{prompt}")
+    
+        # Build a chat prompt template from the system and human message templates
+        chat_prompt_template = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+    
+        # Generate the chat messages using the chat prompt template
+        messages = chat_prompt_template.format_prompt(context=context_content, prompt=prompt_content).to_messages()
+    
+        # Generate the cover letter
+        chat = ChatOpenAI(streaming=True, callbacks=[StreamingStdOutCallbackHandler()], temperature=1)
+        chat(messages)
+    
         # Display the cover letter
-        st.markdown(cover_letter)
+        st.markdown(chat.messages[-1].content)
+
 
 if __name__ == "__main__":
     main()
