21a66aa3e0aae23b52d3c5917f88b94e5876f219
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-05-25 20:48:50 +0000

    Export pdf bug fixes

--- main.py
+++ main.py
@@ -74,15 +74,16 @@ def main():
             # Initialize session state
             if 'text' not in st.session_state:
                 st.session_state['text'] = formatted_content
-
-            # Use the stored value in session state for the text_area
-            st.session_state['text'] = st.text_area("Edit your cover letter:", value=st.session_state['text'], height=600)
+    
+            # Get the current text_area content
+            current_text = st.text_area("Edit your cover letter:", value=st.session_state['text'], height=600)
             if st.form_submit_button('Export Cover Letter'):
+                # Update session state with the current text
+                st.session_state['text'] = current_text
                 # Save the cover letter as a PDF file
                 with open('cover_letter.pdf', 'w') as f:
                     f.write(st.session_state['text'])
                 st.success('Cover letter exported successfully!')
-
 
 if __name__ == "__main__":
     main()
