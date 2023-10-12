3aa4617c83da5600b3d04993343ff75cff709a5c
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-10-12 18:33:32 +0000

    Topic generator

--- main.py
+++ main.py
@@ -19,46 +19,16 @@ def main():
         topic = st.text_input('What topic would you like to write about?')
         
         # 2. Ask the user to Select the type of blog they want to write: Hot Take, Deep Dive, Cross-Topic
-        st.subheader('Select Blog Type')
-        blog_type = st.selectbox('What type of blog do you want to write?', ['Select', 'Hot Take', 'Deep Dive', 'Cross-Topic'])
-        
-        # 3. Supporting Content URLs
-        st.subheader('Enter Supporting Content URLs')
-        url1 = st.text_input('Enter first URL for supporting content:')
-        url1_context = st.text_area('Enter context for first URL:')
-        url2 = st.text_input('Enter second URL for supporting content:')
-        url2_context = st.text_area('Enter context for second URL:')
-    
-        if url1:
-            # Use WebBaseLoader to load the job description
-            loader = WebBaseLoader(url1)
-            url1_pages = loader.load_and_split()
-            url1_text = " ".join([doc.text for doc in url1_pages if hasattr(doc, 'text')]) 
-        else:
-            st.warning('Want a URL?')
-      
-        if url2:
-            # Use WebBaseLoader to load the job description
-            loader = WebBaseLoader(url2)
-            url2_pages = loader.load_and_split()
-            url2_text = " ".join([doc.text for doc in url2_pages if hasattr(doc, 'text')]) 
-        else:
-            st.warning('Want a URL?')
+        st.subheader('Enter System Role')
+        system_role = st.text_input('What type of blog do you want to write?')
       
         # 4. Ask the user if they want to provide any additional notes.
         st.subheader('Enter Additional Notes')
         notes = st.text_area('Any additional notes?')
-    
-    
+      
         if st.button('Generate Topic Ideas'):
             # Prepare the prompt based on the type of blog
-            base_prompt = f"You are a creative senior marketer. I want to write a blog about '{topic}'. I have provided some content that I'd like to use as context to help brainstorm creative blog ideas. This includes an article that is {url1_context} and the supporting content for that article is: {url1_text}. Finally a few additional notes that I would like to incorporate into the ideation of blog ideation is: {notes}. Please provide 3-5"
-            if blog_type == 'Hot Take':
-                prompt = base_prompt + " hot take blog ideas or headlines and provide a few sentence overview about the content for each blog. In which the idea or headline is an opinion or view that goes against the mainstream or could be somewhat taboo."
-            elif blog_type == 'Deep Dive':
-                prompt = base_prompt + " deep dive blog ideas or headlines and provide a few sentence overview about the content for each blog. In which the idea or headline is an exploration of some interesting details related to the topic."
-            elif blog_type == 'Cross-Topic':
-                prompt = base_prompt + " cross-topic blog ideas or headlines and provide a few sentence overview about the content for each blog. In which the idea or headline is an exploration of both the topic and a seemingly irrelevant topic, but one that actually has some interesting connections."
+            prompt = f"I want to create a table of short engaging summaries about events that actually happened but are so unique and out of the ordinary that they are hard to believe they actually happened in real life. The summaries need to be under 100 words and written to retain the readers attention. These real life event summaries should be about '{topic}'. Please return these in a table with the following columns: Title (a title of the story), Year (year the event occured, or an approximate year if the exact date is unknown), Summary (the 100 word or less summary of the event). Please provide 20 event summaries."
         
             # Fetch results using OpenAI API
             response = openai.ChatCompletion.create(
@@ -66,7 +36,7 @@ def main():
               messages=[
                 {
                   "role": "system",
-                  "content": url1_text  
+                  "content": system_role  
                 },
                 {
                   "role": "user",
